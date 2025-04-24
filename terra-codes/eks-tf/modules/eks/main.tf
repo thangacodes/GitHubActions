8e9bfea898ed7f7eb04aadb3919b9955e58d0307
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-24 08:11:29 +0530

    alignment

--- terra-codes/eks-tf/modules/eks/main.tf
+++ terra-codes/eks-tf/modules/eks/main.tf
@@ -1,13 +1,11 @@
 resource "aws_security_group" "eks" {
   vpc_id = var.vpc_id
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "${var.name}-eks-sg"
   }
@@ -15,21 +13,18 @@ resource "aws_security_group" "eks" {
 
 resource "aws_security_group" "nodes" {
   vpc_id = var.vpc_id
-
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "${var.name}-nodes-sg"
   }
@@ -38,7 +33,6 @@ resource "aws_security_group" "nodes" {
 resource "aws_eks_cluster" "this" {
   name     = var.name
   role_arn = var.cluster_role_arn
-
   vpc_config {
     subnet_ids         = var.subnet_ids
     security_group_ids = [aws_security_group.eks.id]
@@ -50,26 +44,21 @@ resource "aws_eks_node_group" "this" {
   node_group_name = "${var.name}-node-group"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids
-
   scaling_config {
     desired_size = var.node_desired
     max_size     = var.node_max
     min_size     = var.node_min
   }
-
   instance_types = var.node_instance_types
-
   remote_access {
     ec2_ssh_key               = var.ssh_key_name
     source_security_group_ids = [aws_security_group.eks.id]
   }
-
   labels = {
     "env"     = "dev"
     "project" = "argocd"
     "owner"   = "thangadurai"
   }
-
   tags = {
     "Name"        = "${var.name}-eks-node"
     "Environment" = "Development"
