bdb075c9604e31e0057d6a712289d0a3aa45ee62
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-24 08:13:02 +0530

    Update main.tf

--- terra-codes/eks-tf/modules/iam/main.tf
+++ terra-codes/eks-tf/modules/iam/main.tf
@@ -1,35 +1,3 @@
-resource "aws_iam_role" "eks" {
-  name = var.eks_role_name
-
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks.name
-  policy_arn = var.eks_policy
-}
-
-resource "aws_iam_role" "nodes" {
-  name = var.node_role_name
-
-  assume_role_policy = data.aws_iam_policy_document.nodes_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "node_policy" {
-  role       = aws_iam_role.nodes.name
-  policy_arn = var.node_policy
-}
-
-resource "aws_iam_role_policy_attachment" "cni_policy" {
-  role       = aws_iam_role.nodes.name
-  policy_arn = var.cni_policy
-}
-
-resource "aws_iam_role_policy_attachment" "registry_policy" {
-  role       = aws_iam_role.nodes.name
-  policy_arn = var.registry_policy
-}
-
 data "aws_iam_policy_document" "eks_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -39,7 +7,6 @@ data "aws_iam_policy_document" "eks_assume_role" {
     }
   }
 }
-
 data "aws_iam_policy_document" "nodes_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -48,4 +15,28 @@ data "aws_iam_policy_document" "nodes_assume_role" {
       identifiers = ["ec2.amazonaws.com"]
     }
   }
+}
+resource "aws_iam_role" "eks" {
+  name = var.eks_role_name
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks.name
+  policy_arn = var.eks_policy
+}
+resource "aws_iam_role" "nodes" {
+  name = var.node_role_name
+  assume_role_policy = data.aws_iam_policy_document.nodes_assume_role.json
+}
+resource "aws_iam_role_policy_attachment" "node_policy" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = var.node_policy
+}
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = var.cni_policy
+}
+resource "aws_iam_role_policy_attachment" "registry_policy" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = var.registry_policy
 }
