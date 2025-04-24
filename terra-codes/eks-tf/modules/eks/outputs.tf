9c183d64573e9e211b814600250cf1b72fb660cc
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-24 08:37:42 +0530

    updated

--- terra-codes/eks-tf/modules/eks/outputs.tf
+++ terra-codes/eks-tf/modules/eks/outputs.tf
@@ -1,12 +1,11 @@
-output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
-output "cluster_arn" {
-  value = aws_eks_cluster.this.arn
-}
 output "ngtags" {
-  value = aws_eks_node_group.this.tags_all
+  value = aws_eks_node_group.argocd.tags_all
 }
+
 output "disk_size" {
-  value = aws_eks_node_group.this.disk_size
+  value = aws_eks_node_group.argocd.disk_size
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.argocd.endpoint
 }
