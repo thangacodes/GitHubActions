27d03e5bd74c9851670103675dae03cefc9c9436
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-25 22:27:02 +0530

    Update prod.sh

--- terra-codes/scripts/prod.sh
+++ terra-codes/scripts/prod.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Print current date and time
-echo "Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
-echo ""
-# Header for staging execution
+echo "Script executed at:" $(date '+%Y-%m-%d %H:%M:%S')
 echo "###################################"
-echo "#        Staging Execution        #"
+echo "#        PRPD Execution           #"
 echo "###################################"
+echo "Get to know about the GH runner information"
+echo "Executor name is:" $(whoami)
+echo "GitHub workflow path is:" $(pwd)
+echo "GitHub hosted machine name is:" $(hostname)
