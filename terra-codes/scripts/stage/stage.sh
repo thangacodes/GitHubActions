22a8e70f9094197d72b17ac51a019e3cef45f734
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-25 23:18:20 +0530

    Update stage.sh

--- terra-codes/scripts/stage/stage.sh
+++ terra-codes/scripts/stage/stage.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-# Print current date and time
-echo "Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
-# Header for staging execution
-echo "###################################"
-echo "#        Stage Execution          #"
-echo "###################################"
-echo "Get to know about the GH runner information"
-echo "Executor name is:" $(whoami)
-echo "GitHub workflow path is:" $(pwd)
-echo "GitHub hosted machine name is:" $(hostname)
+echo "AWS CLI run for S3 related operations..."
+# Fetch AWS region from GitHub repository settings variable
+AWS_REGION="${{ vars.AWS_REGION }}"
+# Check if the variable is not empty
+if [ -z "$AWS_REGION" ]; then
+  echo "Error: AWS_REGION variable is not set."
+  exit 1
+fi
+# Create the S3 bucket
+aws s3api create-bucket \
+  --bucket tfbackend-for-argocd-project \
+  --region "$AWS_REGION" \
+  --create-bucket-configuration LocationConstraint="$AWS_REGION"
+# Create a folder called backend
+aws s3api put-object \
+  --bucket tfbackend-for-argocd-project \
+  --key "backend/" \
+  --region "$AWS_REGION"
+# Get the bucket location
+aws s3api get-bucket-location --bucket tfbackend-for-argocd-project --region "$AWS_REGION"
+
+# # Remove all objects from the S3 bucket
+# aws s3 rm s3://tfbackend-for-argocd-project --recursive --region "$AWS_REGION"
+# # Delete the S3 bucket
+# aws s3api delete-bucket --bucket tfbackend-for-argocd-project --region "$AWS_REGION"
