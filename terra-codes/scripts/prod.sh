2b1137c55d62efd87ac1f24709a30a26011315da
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-25 22:13:56 +0530

    initial commit

---
+++ terra-codes/scripts/prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Print current date and time
+echo "Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
+echo ""
+# Header for staging execution
+echo "###################################"
+echo "#        Staging Execution        #"
+echo "###################################"
