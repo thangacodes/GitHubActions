14c5d3c82b8b01a588a542f145315cd941ec4624
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-26 10:01:08 +0530

    updates on this script

--- terra-codes/scripts/stage/stage.sh
+++ terra-codes/scripts/stage/stage.sh
@@ -4,9 +4,15 @@ echo "Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
 BUCKET_NAME="tfbackend-for-argocd-project"
 REGION="ap-south-1"
 FOLDER="backend"
-read -p "What operation would you like to perform (create or delete): " ACTION
+ACTION=$1
+# read -p "What operation would you like to perform (create or delete): " ACTION
 echo "User entered action is:" $ACTION
 echo " aws cli run for s3 related.."
+if [[ -z "$ACTION" ]]; then
+  echo "Error: No action specified. Use 'create' or 'delete' as the first argument."
+  exit 1
+fi
+echo "User entered action is: $ACTION"
 if [[ $ACTION == "create" ]]; then
   echo "S3 bucket creation in progress..."
   aws s3api create-bucket \
@@ -18,12 +24,16 @@ if [[ $ACTION == "create" ]]; then
     --key "$FOLDER/" \
     --region "$REGION"
   aws s3api get-bucket-location --bucket "$BUCKET_NAME"
-  aws s3 ls "$BUCKET_NAME" "$REGION"
+  aws s3 ls "$BUCKET_NAME" --region "$REGION"
 elif [[ $ACTION == "delete" ]]; then
   echo "S3 bucket deletion in progress..."
   aws s3 ls "$BUCKET_NAME"
   aws s3 rm s3://"$BUCKET_NAME" --recursive --region "$REGION"
   aws s3api delete-bucket --bucket "$BUCKET_NAME" --region "$REGION"
-  aws s3 ls
+  echo "Cross-checking whether the bucket '${BUCKET_NAME}' has been deleted or not"
+  aws s3 ls --region "$REGION"
+else
+  echo "Invalid action: $ACTION. Use 'create' or 'delete'."
+  exit 1
 fi
 exit 0
