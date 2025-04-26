aec678a7ef236bb3147207d939f376884b9ef530
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-26 07:00:51 +0530

    add sgp id finding in outputs.tf

--- terra-codes/eks-tf/modules/eks/outputs.tf
+++ terra-codes/eks-tf/modules/eks/outputs.tf
@@ -1,15 +1,20 @@
-output "ngtags" {
-  value = aws_eks_node_group.argocd.tags_all
-}
-
-output "disk_size" {
-  value = aws_eks_node_group.argocd.disk_size
-}
-
 output "cluster_endpoint" {
   value = aws_eks_cluster.argocd.endpoint
 }
-
 output "cluster_arn" {
   value = aws_eks_cluster.argocd.arn
+}
+output "eksCluster-SGPId" {
+  description = "EKS cluster Security Group ID"
+  value       = aws_security_group.eks.id
+}
+output "eksNodes-SGPId" {
+  description = "EKS nodes custom Security Group ID"
+  value       = aws_security_group.nodes.id
+}
+output "ngtags" {
+  value = aws_eks_node_group.argocd.tags_all
+}
+output "disk_size" {
+  value = aws_eks_node_group.argocd.disk_size
 }
