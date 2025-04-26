39d8fb09257d3959845b8cf46c27746601e23bb7
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-26 06:59:49 +0530

    added security-group id finding

--- terra-codes/eks-tf/outputs.tf
+++ terra-codes/eks-tf/outputs.tf
@@ -14,3 +14,17 @@ output "subnet_ids" {
   description = "IDs of the created subnets"
   value       = module.vpc.subnet_ids
 }
+output "vpc_RT_id" {
+  description = "Route Table Id"
+  value       = module.vpc.routeTable-id
+}
+
+output "SGPId-for-eks" {
+  description = "EKS cluster Security Group Id"
+  value       = module.eks.eksCluster-SGPId
+}
+
+output "SGPId-for-eksNodes" {
+  description = "EKS NodeGroup Security Group Id"
+  value       = module.eks.eksNodes-SGPId
+}
